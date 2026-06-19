a1f4335d56bb9ad0dc3069145c7497036593d2ee
bhavanpoluri5-prog <bhavanpoluri5@gmail.com>
2026-06-19 08:09:35 +0530

    Add files via upload

---
+++ donorService.js
@@ -0,0 +1,20 @@
+const donors = [
+  {
+    id: 1,
+    name: "Rahul",
+    bloodGroup: "A+",
+    age: 22,
+    phone: "9876543210"
+  },
+  {
+    id: 2,
+    name: "Priya",
+    bloodGroup: "O+",
+    age: 25,
+    phone: "9876543211"
+  }
+];
+
+export const getDonors = () => {
+  return donors;
+};
